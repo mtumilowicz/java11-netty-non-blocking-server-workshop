475e0929866dd57c753255d78a35262090b2cec4
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-28 23:47:54 +0100

    EchoClient formatting

--- src/main/java/EchoClient.java
+++ src/main/java/EchoClient.java
@@ -9,8 +9,8 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import java.net.InetSocketAddress;
 
 class EchoClient {
-    private final String host = "localhost";
-    private final int port = 8080;
+
+    private final InetSocketAddress socketAddress = new InetSocketAddress("localhost", 8080);
 
     public static void main(String[] args) throws Exception {
         new EchoClient().start();
@@ -20,34 +20,13 @@ class EchoClient {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap(); // Creates a Bootstrap to create and connect new client channels
-            /*
-            Sets the EventLoopGroup
-            that provides EventLoops for
-            processing Channel events
-             */
-            b.group(group)
-                    /*
-                    Specifies the Channel
-                    implementation to
-                    be used
-                     */
-                    .channel(NioSocketChannel.class)
-                    /*
-                    instead you could directly Connects to the remote host
-                    
-                    */
-                    .remoteAddress(new InetSocketAddress(host, port))
-                    /*
-                    Sets the handler
-                    for Channel events
-                    and data
-                     */
-                    .handler(new ChannelInitializer<SocketChannel>() {
-
+            b.group(group) // set EventLoopGroup that provides EventLoops for processing Channel events
+                    .channel(NioSocketChannel.class) // channel implementation
+                    .remoteAddress(socketAddress)
+                    .handler(new ChannelInitializer<SocketChannel>() { // handler for Channel events and data
                         @Override
                         public void initChannel(SocketChannel ch) {
-                            ch.pipeline().addLast(
-                                    new EchoClientHandler());
+                            ch.pipeline().addLast(new EchoClientHandler());
                         }
                     });
             ChannelFuture f = b.connect().sync();
