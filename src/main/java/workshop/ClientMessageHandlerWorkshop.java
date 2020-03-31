d2525254af7de1b4c8a9c38ff23f1186a3dcbb44
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-31 13:51:06 +0200

    ClientMessageHandlerWorkshop.java hints

--- src/main/java/workshop/ClientMessageHandlerWorkshop.java
+++ src/main/java/workshop/ClientMessageHandlerWorkshop.java
@@ -13,18 +13,18 @@ class ClientMessageHandlerWorkshop extends SimpleChannelInboundHandler<ByteBuf> 
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello Netty!", CharsetUtil.UTF_8));
+        // write and flush Hello Netty!, hint: writeAndFlush, Unpooled.copiedBuffer, CharsetUtil.UTF_8
     }
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
-        System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
-        ctx.close();
+        // print to console: Client received: incoming message, hint:System.out.println, in.toString, CharsetUtil.UTF_8
+        // close connection, hint: ctx.close()
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
+        // print stacktrace, hint: cause.printStackTrace()
+        // close connection, hint: ctx.close()
     }
 }
